a4a140dd300f28f414a72ddfd5b8000a3c965a31
Fran Aguilera <fran.aguilera12@gmail.com>
2025-01-15 00:26:03 -0800

    Reduce scope of changes and go back to original approach of DelayedScheduledExecutorService

--- leakcanary/leakcanary-android-release/src/main/java/leakcanary/BackgroundTrigger.kt
+++ leakcanary/leakcanary-android-release/src/main/java/leakcanary/BackgroundTrigger.kt
@@ -3,23 +3,54 @@ package leakcanary
 import android.app.Application
 import leakcanary.internal.BackgroundListener
 import leakcanary.internal.friendly.checkMainThread
-
-import leakcanary.AnalysisJobHandler.JobState.STARTED
-import leakcanary.AnalysisJobHandler.JobState.STOPPED
+import shark.SharkLog
+import java.util.concurrent.Executor
 
 class BackgroundTrigger(
   private val application: Application,
-  private val analysisJobHandler: AnalysisJobHandler,
+  private val analysisClient: HeapAnalysisClient,
+  /**
+   * The executor on which the analysis is performed and on which [analysisCallback] is called.
+   * This should likely be a single thread executor with a background thread priority.
+   */
+  private val analysisExecutor: Executor,
+
   processInfo: ProcessInfo = ProcessInfo.Real,
+
+  /**
+   * Called back with a [HeapAnalysisJob.Result] after the app has entered background and a
+   * heap analysis was attempted. This is called on the same thread that the analysis was
+   * performed on.
+   *
+   * Defaults to logging to [SharkLog] (don't forget to set [SharkLog.logger] if you do want to see
+   * logs).
+   */
+  private val analysisCallback: (HeapAnalysisJob.Result) -> Unit = { result ->
+    SharkLog.d { "$result" }
+  },
 ) {
 
+  @Volatile
+  private var currentJob: HeapAnalysisJob? = null
+
   private val backgroundListener = BackgroundListener(processInfo) { appInBackgroundNow ->
-    val jobState = if (appInBackgroundNow) {
-      STARTED
+    if (appInBackgroundNow) {
+      check(currentJob == null) {
+        "Current job set to null when leaving background"
+      }
+
+      val job =
+        analysisClient.newJob(JobContext(BackgroundTrigger::class))
+      currentJob = job
+      analysisExecutor.execute {
+        val result = job.execute()
+        currentJob = null
+        analysisCallback(result)
+      }
     } else {
-      STOPPED
+      currentJob?.cancel("app left background")
+      currentJob = null
     }
-    analysisJobHandler.updateJobState(BackgroundTrigger::class, jobState)
   }
 
   fun start() {
--- leakcanary/leakcanary-android-release/src/main/java/leakcanary/ScreenOffTrigger.kt
+++ leakcanary/leakcanary-android-release/src/main/java/leakcanary/ScreenOffTrigger.kt
@@ -8,26 +8,68 @@ import android.content.Intent.ACTION_SCREEN_OFF
 import android.content.Intent.ACTION_SCREEN_ON
 import android.content.IntentFilter
 import android.os.Build
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
 import leakcanary.internal.friendly.checkMainThread
-import leakcanary.AnalysisJobHandler.JobState.STARTED
-import leakcanary.AnalysisJobHandler.JobState.STOPPED
+import shark.SharkLog
 
 class ScreenOffTrigger(
   private val application: Application,
-  private val analysisJobHandler: AnalysisJobHandler
+  private val analysisClient: HeapAnalysisClient,
+  /**
+   * The executor on which the analysis is performed and on which [analysisCallback] is called.
+   * This should likely be a single thread executor with a background thread priority.
+   */
+  analysisExecutor: Executor,
+
+  /**
+   * Called back with a [HeapAnalysisJob.Result] after the screen went off and a
+   * heap analysis was attempted. This is called on the same thread that the analysis was
+   * performed on.
+   *
+   * Defaults to logging to [SharkLog] (don't forget to set [SharkLog.logger] if you do want to see
+   * logs).
+   */
+  private val analysisCallback: (HeapAnalysisJob.Result) -> Unit = { result ->
+    SharkLog.d { "$result" }
+  },
+
+  /**
+   * Initial delay to wait for analysisExecutor to start analysis
+   *
+   * If not provided, initial delay is 500ms
+   */
+  private val analysisExecutorDelayMillis: Long = INITIAL_EXECUTOR_DELAY_IN_MILLI
 ) {
 
+  private var currentJob = AtomicReference<HeapAnalysisJob?>()
+
+  private val delayedScheduledExecutorService = DelayedScheduledExecutorService()
   private val screenReceiver = object : BroadcastReceiver() {
     override fun onReceive(
       context: Context,
       intent: Intent
     ) {
-      val jobState = if (intent.action == ACTION_SCREEN_OFF)
-        STARTED
-      else {
-        STOPPED
+      if (intent.shouldStartAnalysis()) {
+          val job =
+            analysisClient.newJob(JobContext(ScreenOffTrigger::class))
+          if(currentJob.compareAndSet(null, job)){
+            delayedScheduledExecutorService.schedule {
+              val result = job.execute()
+              analysisCallback(result)
+            }
+          }
+      } else {
+        delayedScheduledExecutorService.cancel()
+        currentJob.getAndUpdate { job ->
+          job?.cancel("screen on again")
+          null
+        }
       }
-      analysisJobHandler.updateJobState(ScreenOffTrigger::class, jobState)
     }
   }
 
@@ -48,6 +90,39 @@ class ScreenOffTrigger(
   fun stop() {
     checkMainThread()
     application.unregisterReceiver(screenReceiver)
-    analysisJobHandler.shutdown()
+  }
+
+  private fun Intent.shouldStartAnalysis():Boolean{
+    return this.action == ACTION_SCREEN_OFF && currentJob.get() == null
+  }
+
+  private class DelayedScheduledExecutorService(
+    private val analysisExecutorDelayMillis:Long,
+    private val analysisExecutor: Executor){
+
+    private var scheduledFuture:ScheduledFuture<*>?=null
+
+    private val scheduledExecutorService:ScheduledExecutorService by lazy {
+      Executors.newScheduledThreadPool(1)
+    }
+
+    /** Runs the specified [action] after an initial [analysisExecutorDelayMillis] */
+    fun schedule(action:Runnable){
+      scheduledFuture =
+        scheduledExecutorService.schedule(
+          { analysisExecutor.execute(action)},
+          analysisExecutorDelayMillis,
+          TimeUnit.MILLISECONDS
+        )
+    }
+
+    /** Cancels prior ScheduledExecutorService */
+    fun cancel(){
+      scheduledFuture?.cancel(true)
+    }
+  }
+
+  companion object{
+    const val INITIAL_EXECUTOR_DELAY_IN_MILLI = 500L
   }
 }
